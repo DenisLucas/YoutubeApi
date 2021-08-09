15d081bf1234fae0f706fb41a10adf24b6ea04d2
Denis <denislucasvalverdesilva@gmail.com>
2021-08-08 21:44:30 -0400

    1.0

---
+++ YoutubeApi/Installer/DBInstaller.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace YoutubeApi.Installer
+{
+    public class DBInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            return;
+        }
+    }
+}
